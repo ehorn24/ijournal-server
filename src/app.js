46e1fa5f2bf469a97a2880dc6d6a5edf7090a1e2
Ella Horn <ellalouisahorn@yahoo.com>
2020-09-07 14:46:47 -0500

    add userRouter

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const { CLIENT_ORIGIN } = require("./config");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
 
@@ -9,9 +10,17 @@ const app = express();
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
+const userRouter = require("./users/user-router");
+
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN,
+  })
+);
+
+app.use("/api/users", userRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
---
+++ src/users/user-router.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const UserService = require("./user-service");
+const md5 = require("md5");
+
+const userRouter = express.Router();
+const jsonParser = express.json();
+
+userRouter
+  .route("/")
+  .get((req, res, next) => {
+    const knexInst = req.app.get("db");
+    UserService.getAllUsers(knexInst)
+      .then((users) => {
+        res.status(200).json(users);
+      })
+      .catch(next);
+  })
+  .post(jsonParser, (req, res, next) => {
+    const knexInst = req.app.get("db");
+    console.log(req.body);
+    const newUser = {
+      username: req.body.username,
+      password: md5(req.body.password),
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+    };
+    for (const [key, value] of Object.entries(newUser)) {
+      if (value == null) {
+        return res.status(400).json({
+          error: { message: `Missing ${key} from request body` },
+        });
+      }
+    }
+    UserService.createNewUser(knexInst, newUser)
+      .then((user) => {
+        res.status(201).json(user);
+      })
+      .catch(next);
+  });
+
+userRouter.route("/login").post(jsonParser, (req, res, next) => {
+  const knexInst = req.app.get("db");
+  const { username, password } = req.body;
+  const retUser = { username, password: md5(password) };
+  UserService.authUser(knexInst, retUser.username)
+    .then((user) => {
+      if (!user) {
+        res.status(404).json({ message: `User ${username} does not exist` });
+      } else if (
+        user.username === retUser.username &&
+        user.password === retUser.password
+      ) {
+        res.status(200).json({ message: "Logged in!" });
+      } else {
+        res.status(404).json({ message: "Invalid credentials" });
+      }
+      res.end();
+    })
+    .catch(next);
+});
+
+userRouter.route("/:id").get((req, res, next) => {
+  const knexInst = req.app.get("db");
+  const id = req.params.id;
+  UserService.getUserById(knexInst, id)
+    .then((user) => {
+      if (!user) {
+        res
+          .status(404)
+          .json({ error: { message: `User with ID ${id} does not exist` } });
+      }
+      res.status(200).json(user);
+    })
+    .catch(next);
+});
+
+module.exports = userRouter;
