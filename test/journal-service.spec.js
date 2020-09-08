2d3153ccf4683c883a7442d11183d6fade49e686
Ella Horn <ellalouisahorn@yahoo.com>
2020-09-07 19:21:21 -0500

    journal endpoint testing

---
+++ test/journal-service.spec.js
@@ -0,0 +1,41 @@
+const knex = require("knex");
+const app = require("../src/app");
+const { expect } = require("chai");
+const supertest = require("supertest");
+
+describe("Journal endpoints", () => {
+  let db;
+  before("make knex instance", () => {
+    db = knex({
+      client: "pg",
+      connection: process.env.TEST_DATABASE_URL,
+    });
+    app.set("db", db);
+  });
+
+  after("disconnect from db", () => {
+    db.destroy();
+  });
+
+  it("GET /u/:user_id responds with 200 and all the user's journals", () => {
+    return supertest(app).get("/api/journals/u/1").expect(200);
+  });
+
+  it("GET /u/:user_id returns 404 if the user does not exist or the user has no journals", () => {
+    return supertest(app).get("/api/journals/u/10").expect(404);
+  });
+
+  it.only("GET /j/:journal_id returns 200 and an array with the journal info", () => {
+    return supertest(app)
+      .get("/api/journals/j/2")
+      .expect(200)
+      .then((res) => {
+        expect(res.body).to.have.all.keys(
+          "id",
+          "owner",
+          "journal_name",
+          "date_created"
+        );
+      });
+  });
+});
