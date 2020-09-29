d55e6d9c60473eb2fff5548df4a88b65015a73c0
Ella Horn <ellalouisahorn@yahoo.com>
2020-09-28 21:14:35 -0500

    added testing for new entry endpoints

--- test/entry-service.spec.js
+++ test/entry-service.spec.js
@@ -17,7 +17,44 @@ describe("Entry endpoints", () => {
     db.destroy();
   });
 
-  it.only("GET /u/:user_id returns 200 and all the user's entries", () => {
+  it("GET /u/:user_id returns 200 and all the user's entries", () => {
     return supertest(app).get("/api/entries/u/1").expect(200);
+  });
+
+  it("POST /u/:user_id returns 400 if entry post is not successful", () => {
+    let data = {
+      entry_title: "Test Title",
+      tags: ["tag 1", "tag 2"],
+      entry_text: "Test Text",
+    };
+    return supertest(app).post("/api/entries/u/1").send(data).expect(400);
+  });
+
+  it("POST /u/:user_id returns 201 and the new entry if successful", () => {
+    let data = {
+      journal: 1,
+      entry_title: "Test Title",
+      tags: ["tag 1", "tag 2"],
+      entry_text: "Test Text",
+    };
+    return supertest(app)
+      .post("/api/entries/u/1")
+      .send(data)
+      .expect(201)
+      .then((res) => {
+        expect(res.body).to.have.all.keys(
+          "id",
+          "owner",
+          "journal",
+          "date_created",
+          "entry_title",
+          "tags",
+          "entry_text"
+        );
+      });
+  });
+
+  it.only("DELETE /:entry_id responds with 204 if entry successfully deleted", () => {
+    return supertest(app).delete("/api/entries/3").expect(204);
   });
 });
