f8ad6b3742d65f51a594451d14f572579c48bd36
Ella Horn <ellalouisahorn@yahoo.com>
2020-09-28 21:39:52 -0500

    added testing for journal endpoints

--- test/journal-service.spec.js
+++ test/journal-service.spec.js
@@ -38,4 +38,37 @@ describe("Journal endpoints", () => {
         );
       });
   });
+
+  it("PATCH /j/:journal_id responds with 400 if no edit fields are present", () => {
+    return supertest(app).patch("/api/journals/j/1").expect(400);
+  });
+
+  it("DELETE /j/:journal_id responds with 204 if the journal is successfully deleted", () => {
+    return supertest(app).delete("/api/journals/j/1").expect(204);
+  });
+
+  it("POST /u/:user_id responds with 400 if a key is missing from the request body", () => {
+    let data = {
+      dumdum: "alejfaleifj",
+    };
+    return supertest(app).post("/api/journals/u/1").send(data).expect(400);
+  });
+
+  it("POST /u/:user_id responds with 201 and an array with the new journal info if successful", () => {
+    let data = {
+      journal_name: "Waterbottle",
+    };
+    return supertest(app)
+      .post("/api/journals/u/1")
+      .send(data)
+      .expect(201)
+      .then((res) => {
+        expect(res.body).to.have.all.keys(
+          "id",
+          "owner",
+          "journal_name",
+          "date_created"
+        );
+      });
+  });
 });
