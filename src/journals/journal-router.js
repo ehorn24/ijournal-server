da808076e5f68c2081e32c7e387e1c7fdf3c94fc
Ella Horn <ellalouisahorn@yahoo.com>
2020-09-28 20:39:19 -0500

    add edit journal to router

--- src/journals/journal-router.js
+++ src/journals/journal-router.js
@@ -4,20 +4,44 @@ const JournalService = require("./journal-service");
 const journalRouter = express.Router();
 const jsonParser = express.json();
 
-journalRouter.route("/j/:journal_id").get((req, res, next) => {
-  const knexInst = req.app.get("db");
-  const journal_id = req.params.journal_id;
-  JournalService.getJournalById(knexInst, journal_id)
-    .then((journal) => {
-      if (!journal) {
-        res.status(404).json({
-          error: { message: `Journal with ID ${journal_id} does not exist` },
-        });
-      }
-      res.status(200).json(journal);
-    })
-    .catch(next);
-});
+journalRouter
+  .route("/j/:journal_id")
+  .get((req, res, next) => {
+    const knexInst = req.app.get("db");
+    const journal_id = req.params.journal_id;
+    JournalService.getJournalById(knexInst, journal_id)
+      .then((journal) => {
+        if (!journal) {
+          res.status(404).json({
+            error: { message: `Journal with ID ${journal_id} does not exist` },
+          });
+        }
+        res.status(200).json(journal);
+      })
+      .catch(next);
+  })
+  .patch(jsonParser, (req, res, next) => {
+    const knexInst = req.app.get("db");
+    const { journal_name, journal_cover } = req.body;
+    const itemToUpdate = {
+      journal_name: journal_name !== "" ? journal_name : undefined,
+      journal_cover: journal_cover !== "" ? journal_cover : undefined,
+    };
+    const numVals = Object.values(itemToUpdate).filter(Boolean).length;
+    if (numVals === 0) {
+      return res.status(400).json({
+        error: {
+          message: `Request body must contain either journal name or journal cover`,
+        },
+      });
+    } else {
+      JournalService.editJournal(knexInst, req.params.journal_id, itemToUpdate)
+        .then((x) => {
+          res.status(204).end();
+        })
+        .catch(next);
+    }
+  });
 
 journalRouter
   .route("/u/:user_id")
